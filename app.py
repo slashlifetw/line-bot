8ac18017a3db79c2c0b423c7199f7b5f3660483a
slashlifetw <dagancivil1931@gmail.com>
2023-02-05 18:04:50 +0800

    update

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def handle_message(event):
     msg = event.message.text
     r = '我看不懂你說什麼'
 
-    if msg == ['hi', 'Hi']:
+    if msg in ['hi', 'Hi']:
         r = '嗨'
     elif msg == '你吃飯了嗎':
         r = '還沒'
@@ -51,7 +51,10 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        StickerSendMessage(
+    package_id='1',
+    sticker_id='1'
+)
 
 
 if __name__ == "__main__":
